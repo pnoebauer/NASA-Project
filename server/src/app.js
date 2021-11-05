17a123330f883ca32c17faf594135b99e5a6b060
Philip Noebauer <pnoebauer>
2021-11-05 11:09:52 +0800

    built API for frontend to get planets from the backend

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cors = require('cors');
+
 const morgan = require('morgan');
 
 const {planetsRouter} = require('./routes/planets/planets.router');
@@ -6,7 +8,8 @@ const {planetsRouter} = require('./routes/planets/planets.router');
 const app = express();
 
 app.use(morgan('tiny'));
+app.use(cors());
 app.use(express.json());
 app.use(planetsRouter);
 
-module.exports = {app};
+module.exports = app;
