6eda23da0064465ebdb720167c46a14dd91e982e
Philip Noebauer <pnoebauer>
2021-11-06 09:30:42 +0800

    loading data into planets model on server startup

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,48 @@
-const planets = [];
+const parse = require('csv-parse');
+const fs = require('fs');
 
-module.exports = planets;
+const path = require('path');
+
+const filePath = path.join(__dirname, '..', '..', 'data', 'kepler_data.csv');
+
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+	return (
+		planet['koi_disposition'] === 'CONFIRMED' &&
+		planet['koi_insol'] > 0.36 &&
+		planet['koi_insol'] < 1.11 &&
+		planet['koi_prad'] < 1.6
+	);
+}
+
+function loadPlanetsData() {
+	return new Promise((resolve, reject) => {
+		fs.createReadStream(filePath)
+			.pipe(
+				parse({
+					comment: '#',
+					columns: true, //returns each row as JS object rather than everything lumped into one array
+				})
+			)
+			.on('data', data => {
+				if (isHabitablePlanet(data)) {
+					habitablePlanets.push(data);
+				}
+			})
+			.on('error', err => {
+				console.log(err);
+				return reject(err);
+			})
+			.on('end', () => {
+				// const planetNames = habitablePlanets.map(planet => planet['kepler_name']);
+				// console.log(planetNames);
+				// console.log(`${habitablePlanets.length} habitable planets found`); //data will be raw buffers of bytes if no pipe is used
+
+				// return resolve(planetNames);
+				return resolve();
+			});
+	});
+}
+
+module.exports = {loadPlanetsData, planets: habitablePlanets};
--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,24 @@
 const http = require('http');
 const app = require('./app');
 
+const {loadPlanetsData} = require('./models/planets.model');
+
 const port = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-server.listen(port, () => {
-	console.log(`Server is running on port:${port}`);
-});
+// Loading data on startup -------------------------
+// wrapper to enable await
+async function startServer() {
+	await loadPlanetsData(); //make sure that data is loaded before starting up the server
+
+	server.listen(port, () => {
+		console.log(`Server is running on port:${port}`);
+	});
+}
+// --------------------------------------------------
+
+startServer();
 
 // shorthand version of passing it into server
 // same as http.createServer(app).listen
