7affb08006bbb64b0cf85b2b053ce0d76c4a3de0
Philip Noebauer <pnoebauer>
2021-11-11 14:21:21 +0800

    functionality for submitting new launches added

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -14,9 +14,24 @@ async function httpGetLaunches() {
 	return data.sort((a, b) => a.flightNumber - b.flightNumber);
 }
 
+// Submit given launch data to launch system.
 async function httpSubmitLaunch(launch) {
-	// TODO: Once API is ready.
-	// Submit given launch data to launch system.
+	try {
+		const response = await fetch(`${API_URL}/launches`, {
+			method: 'POST', // *GET, POST, PUT, DELETE, etc.
+			mode: 'cors', // no-cors, *cors, same-origin
+			cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+			credentials: 'same-origin', // include, *same-origin, omit
+			headers: {'Content-Type': 'application/json'},
+			redirect: 'follow', // manual, *follow, error
+			referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+			body: JSON.stringify(launch), // body data type must match "Content-Type" header
+		});
+
+		return response;
+	} catch (e) {
+		return {ok: false};
+	}
 }
 
 async function httpAbortLaunch(id) {
--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -7,8 +7,22 @@ function httpGetAllLaunches(req, res) {
 function httpPostLaunch(req, res) {
 	const launch = req.body;
 
-	launch.launchDate = new Date(launch.launchDate);
+	const {launchDate, mission, rocket, target} = launch;
 
+	if (!launchDate || !mission || !rocket || !target) {
+		return res.status(400).json({
+			error: 'Missing required launch property',
+		});
+	}
+
+	launch.launchDate = new Date(launch.launchDate);
+	// if (launch.launchDate.toString() === 'Invalid Date') {
+	//isNaN(launch.launchDate.valueOf()) in the background (if valid, valueOf will return ms unix format)
+	if (isNaN(launch.launchDate)) {
+		return res.status(400).json({
+			error: 'Invalid launch date',
+		});
+	}
 	addNewLaunch(launch);
 
 	return res.status(201).json(launch);
